8f000ffee1a9d5b8728ab850a6da56c851449c2c
Julio Luna <julio.luna@mdp.com.pe>
2020-07-23 11:09:02 -0500

    jblv | quitar archivos innecesarios

--- Asistencia/Asistencia.Web/Controllers/HomeController.cs
+++ Asistencia/Asistencia.Web/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Asistencia.Web.Models;
 
 namespace Asistencia.Web.Controllers
 {
@@ -28,10 +27,5 @@ namespace Asistencia.Web.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
